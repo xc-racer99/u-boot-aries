cc79135f3f2471c1416980d53deaedef7c60548c
Jonathan Bakker <xc-racer2@live.ca>
2019-03-06 11:45:57 -0800

    Add some comments in on different paths and what works
    Still can't figure out why OneNAND doesn't appear to work, it will read just fine once we're in u-boot proper

--- board/samsung/aries/spl_build.c
+++ board/samsung/aries/spl_build.c
@@ -32,9 +32,11 @@ static void setup_global_data(gd_t *gdp)
 }
 
 extern void do_ibl(void);
+extern void do_lowlevel_init(void);
 
 void (*start_usb_boot)(void) = (void *) 0xD0007D78;
 void (*init_system)(void) = (void *) 0xD0007CAC;
+void (*irom_enable_interrupts)(void) = (void *) 0xD000C694;
 
 void board_init_f(unsigned long bootflag)
 {
@@ -48,34 +50,46 @@ void board_init_f(unsigned long bootflag)
 	reg |= 0x301;
 	writel(reg, S5PC110_PS_HOLD_CTRL);
 
-#if 1
+#if 0
+	/* Use blob init_by_rebell to initialize memory and clocks
+	 * Results in working display */
 	do_ibl();
 
+	/* Clears interrupts and enable them for SBL from USB loading */
+	init_system();
+#else
+	/* Use u-boot's clock and memory init
+	 * Display doesn't work in SBL
+	 */
+	do_lowlevel_init();
+
+	/* Enables interrupts for SBL from USB loading */
+	irom_enable_interrupts();
+#endif
+
+#if 0
+	/* Load stock SBL from OneNAND
+	 * Currently not working for some reason
+	 */
 	onenand_spl_load_image(0xa80000,
 			0x140000, (void *) 0x40244000);
 
-#if 0
-	init_system();
-
+	/* Jump to U-Boot image */
+	uboot = (void *)0x40244000;
+#elif 1
+	/* Load SBL from USB, aka HIBL */
 	memcpy((void *)0xD0035400, (void *)0xD000C90C, 0x70);
 
 	start_usb_boot();
 	uboot = (void *)readl(0xD00354D0);
-#endif
 #else
-#if 0
-	if (do_lowlevel_init())
-		power_exit_wakeup();
-
-	copy_uboot_to_ram();
-#else
+	/* Load u-boot directly */
 	onenand_spl_load_image(CONFIG_SYS_ONENAND_U_BOOT_OFFS,
 			CONFIG_BOARD_SIZE_LIMIT, (void *) CONFIG_SYS_TEXT_BASE);
-#endif
 
-#endif
 	/* Jump to U-Boot image */
-	uboot = (void *)0x40244000;
+	uboot = (void *)CONFIG_SYS_TEXT_BASE;
+#endif
 
 	(*uboot)();
 	/* Never returns Here */
