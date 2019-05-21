701d8df92a97a2200ac85c69e7e404fb9855387f
Jonathan Bakker <xc-racer2@live.ca>
2019-05-20 20:12:42 -0700

    s5pc1xx: Update s5pc110 FIMD configuration
    Make sure the power domain is on, correct the clock config

--- arch/arm/mach-s5pc1xx/system.c
+++ arch/arm/mach-s5pc1xx/system.c
@@ -14,12 +14,29 @@ static void s5pc110_set_system_display(void)
 {
 	int i;
 
+	/* Enable power domain */
+	if(!(readl(S5PC110_BLK_PWR_STAT) & 0x1 << 3)) {
+		setbits_le32(S5PC110_NORMAL_CFG, 0x1 << 3);
+
+		/* Wait for update */
+		while(!(readl(S5PC110_BLK_PWR_STAT) & 0x1 << 3))
+			mdelay(1);
+	}
+
+	/**
+	 * Set GPF0 as LVD_HSYNC,VSYNC,VCLK,VDEN,VD[3:0]
+	 * Set GPF1 as VD[11:4]
+	 * Set GPF2 as VD[19:12]
+	 * Set GPF3 as VD[23:20]
+	 * Disable pulls, drive strength to max
+	 */
 	for (i = S5PC110_GPIO_F00; i < S5PC110_GPIO_F34; i++) {
 		gpio_cfg_pin(i, S5P_GPIO_FUNC(2));
 		gpio_set_pull(i, S5P_GPIO_PULL_NONE);
 		gpio_set_drv(i, S5P_GPIO_DRV_4X);
 	}
 
+	/* Set everything to go to FIMD */
 	writel(0x2, S5PC110_MDNIE_SEL);
 }
 
